b6cc3d714192bbd5266f8fe68779a3e65e350790
Liuxudong <17801036039@163.com>
2017-09-25 00:20:31 +0800

    反向传播啊。。。。

--- DeepNeuralNetwork/scripts/dnn2.py
+++ DeepNeuralNetwork/scripts/dnn2.py
@@ -7,40 +7,48 @@ from DeepNeuralNetwork.scripts.util_activations import sigmoid, relu, sigmoid_ba
 
 
 class DNN2(object):
-    def __init__(self, layers_dims, learning_rate=0.05, iter_num=10):
+    def __init__(self, layers_dims, learning_rate=0.005, iter_num=1, activate_func=None):
         """
         init the object.
         :param layers_dims: list, layers of every layer, layers_dims[0] is the input dim and the last is output dim
         """
         self.learning_rate_ = learning_rate
         self.iter_num_ = iter_num
-        self.layer_dims_ = layers_dims             # list the i'st  layer dims
-        self.num_layers_ = len(self.layer_dims_)
+        self.layer_dims_ = layers_dims                 # list the i'st  layer dims
+        self.num_layers_ = len(self.layer_dims_) - 1   # hide layer number
+        self.activation_func_ = activate_func          # activations
 
         self.parameters_ = {}
-
         self.layer_cache_Z_ = {}
-        self.layer_out_ = []          # A0=X, A1, A2,... AL
+        self.layer_out_A_ = {}          # A0=X, A1, A2,... AL
 
     def _initializer(self):
         """
         initialize parameters deep
         :return:
         """
-        np.random.seed(3)
-        for l in range(self.num_layers_):
-            self.parameters_['W' + str(l)] = (np.random.rand(self.layer_dims_[l], self.layer_dims_[l-1]) - 0.5) * 0.01
+        if self.activation_func_ is None:
+            self.activation_func_ = ['relu'] * self.num_layers_
+            self.activation_func_[-1] = 'sigmoid'
+
+        np.random.seed(4)
+        for l in range(1, self.num_layers_ + 1):
+            self.parameters_['W' + str(l)] = (np.random.rand(self.layer_dims_[l], self.layer_dims_[l-1])) * 0.01
             self.parameters_['b' + str(l)] = np.zeros((self.layer_dims_[l], 1))
 
-            # assert (self.parameters_['W' + str(l)]) == (self.layer_dims_[l], self.layer_dims_[l-1])
-            # assert (self.parameters_['b' + str(l)]) == (self.layer_dims_[l], 1)
+            assert (self.parameters_['W' + str(l)].shape == (self.layer_dims_[l], self.layer_dims_[l-1]))
+            assert (self.parameters_['b' + str(l)].shape == (self.layer_dims_[l], 1))
+
+
 
     def _linear_forward(self, layer):
         """
         Implement the linear part of a layer's forward propation
         :return:
         """
-        Z = np.dot(self.parameters_['W' + str(layer)], self.layer_out_[layer]) + self.parameters_['b' + str(layer)]
+        # Z^[L] = W^[L] * A^[L-1] + b^[L]
+        Z = np.dot(self.parameters_['W' + str(layer)], self.layer_out_A_['A' + str(layer - 1)]) + self.parameters_['b' + str(layer)]
+        self.layer_cache_Z_['Z' + str(layer)] = Z
         # assert (Z.shape == self.parameters_['W' + str(layer)].shape[0], self.layer_out_[layer].shape[1])
         return Z
 
@@ -53,24 +61,22 @@ class DNN2(object):
         return A
 
     def _l_model_forward(self, A):
-        for l in range(1, self.num_layers_):
-            A_prev = A
-            Z = self._linear_forward(l)
-            self.layer_cache_Z_['layer_' + str(l)] = Z
-            A = self._linear_activation_forward(Z, 'relu')
+        self.layer_out_A_['A0'] = A   # self.layer_out_[0] = A0 = X
+        for l in range(self.num_layers_):
+            Z = self._linear_forward(l + 1)
+            # print("Z is: \n", Z)
+            A = self._linear_activation_forward(Z, self.activation_func_[l])
+            print("with activation " + self.activation_func_[l] + "A is: \n", A)
+            self.layer_out_A_['A' + str(l + 1)] = A
 
-        ZL = self._linear_forward(self.num_layers_)
-        self.layer_cache_Z_['layer_' + str(self.num_layers_)] = ZL
-        AL = self._linear_activation_forward(ZL, 'sigmoid')
-
-        assert (AL.shape == (self.layer_dims_[-1], X.shape[1]))
-
-        return AL
+        assert (self.layer_out_A_['A' + str(self.num_layers_)].shape == (self.layer_dims_[-1], A.shape[1]))
+        # print("AL is: " + "--" * 10 + "\n", self.layer_out_A_['A' + str(self.num_layers_)])
+        return self.layer_out_A_['A' + str(self.num_layers_)]
 
     @staticmethod
     def _compute_cost(AL, Y):
         m = Y.shape[1]
-        cost = np.sum(np.multiply(Y, np.log(AL)) + np.multiply((1 - Y), np.log(1 - AL)), axis=-1) / m
+        cost = - np.sum(np.multiply(Y, np.log(AL)) + np.multiply((1 - Y), np.log(1 - AL)), axis=-1) / m
         cost = np.squeeze(cost)
         return cost
 
@@ -83,30 +89,8 @@ class DNN2(object):
         """
         return -np.divide(Y, AL) + np.divide(1 - Y, 1 - AL)
 
-    def _linear_backward(self, dZ, layer):
-        """
-        Implement the linear portion of backward propagation
-        :param dZ: Gradient of the cost with respect to the linear output (of the current layer l)
-        :layer:
-        :return:
-        """
-        A_prev = self.layer_out_[layer - 1]
-        w = self.parameters_['W' + str(layer)]
-        b = self.parameters_['b' + str(layer)]
-        m = A_prev.shape[1]
-
-        dw = np.matmul(dZ, A_prev.T) / m
-        db = np.sum(dZ, axis=-1, keepdims=True) / m
-        dA_prev = np.matmul(w.T, dZ)
-
-        assert (dA_prev.shape == A_prev.shape)
-        assert (dw.shape == w.shape)
-        assert (db.shape == b.shape)
-
-        return dA_prev
-
     @staticmethod
-    def _linear_activation_backward(self, dA, Z, activation):
+    def _linear_activation_backward(dA, Z, activation):
         """
         Implement the backward propagation for the LINEAR->ACTIVATION layer
         :param activation:
@@ -127,15 +111,22 @@ class DNN2(object):
         :return:
         """
         grads = {}
-        Y = Y.shape(AL.shape)
+        Y = Y.reshape(AL.shape)
+        m = Y.shape[1]
         dAl = self._compute_cost_backpropagation(AL, Y)
 
-        for l in reversed(range(self.num_layers_ - 1)):
-            dZ = self._linear_activation_backward(dAl, self.layer_cache_Z_['layer_' + str(l + 1)])
-            grads['dW' + str(l + 1)] = np.matmul(dZ, dAl.T) / self.layer_dims_[l + 1]
-            grads['db' + str(l + 1)] = np.sum(dZ, axis=-1, keepdim=True) / self.layer_dims_[l + 1]
+        for l in reversed(range(1, self.num_layers_ + 1)):
+            dZl = self._linear_activation_backward(dA=dAl, Z=self.layer_cache_Z_['Z' + str(l)], activation=self.activation_func_[l - 1])
+            dWl = np.matmul(dZl, self.layer_out_A_['A' + str(l - 1)].T) / m
+            dbl = np.sum(dZl, axis=-1, keepdims=True) / m
             # dAl backpropagation
-            dAl = np.matmul(grads['dW' + str(l + 1)].T, dZ)
+            dAl = np.matmul(dWl.T, dZl)
+
+            assert (dWl.shape == self.parameters_['W' + str(l)].shape)
+            assert (dbl.shape == self.parameters_['b' + str(l)].shape)
+
+            grads['dW' + str(l)] = dWl
+            grads['db' + str(l)] = dbl
 
         return grads
 
@@ -145,7 +136,7 @@ class DNN2(object):
         :param grads:
         :return:
         """
-        for l in self.num_layers_:
+        for l in range(1, self.num_layers_ + 1):
             self.parameters_['W' + str(l)] -= self.learning_rate_ * grads['dW' + str(l)]
             self.parameters_['b' + str(l)] -= self.learning_rate_ * grads['db' + str(l)]
 
@@ -167,7 +158,6 @@ class DNN2(object):
             self._update_parameters(grads)
             print("Iter #" + str(i) + ", cost is: " + str(cost))
 
-
     def score(self, X, Y):
         """
         Score
@@ -175,7 +165,7 @@ class DNN2(object):
         :param Y:
         :return:
         """
-        self.predict(X) == Y
+        return self.predict(X) == Y
 
     def predict(self, X):
         """
--- DeepNeuralNetwork/scripts/train_and_test.py
+++ DeepNeuralNetwork/scripts/train_and_test.py
@@ -24,8 +24,10 @@ test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
 train_set_x = train_set_x_flatten / 255
 test_set_x = test_set_x_flatten / 255
 
-dnn = DNN2(layers_dims=[12288, 10, 1])
+dnn = DNN2(layers_dims=[12288, 10, 1], iter_num=2)
 dnn.fit(train_set_x, train_set_y)
+# print(dnn.predict(train_set_x))
+# print(dnn.score(train_set_x, train_set_y))
 
 # lr = LogisticRegression(num_iterations=4000, learning_rate=0.002, print_cost=True)
 # lr.fit(train_set_x, train_set_y)
